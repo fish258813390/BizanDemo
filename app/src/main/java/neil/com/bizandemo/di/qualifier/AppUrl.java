dcd39e44f1aa1b1b0e72416ccc05f23efbcd9c2b
fish258813390 <258813390@qq.com>
2018-03-13 23:06:24 +0800

    1.优化

--- app/src/main/java/neil/com/bizandemo/di/qualifier/AppUrl.java
+++ app/src/main/java/neil/com/bizandemo/di/qualifier/AppUrl.java
@@ -7,10 +7,10 @@ import java.lang.annotation.RetentionPolicy;
 import javax.inject.Qualifier;
 
 /**
- * App 注解
+ * App 注解网络层
+ * RetrofitHelper 依赖注入
  * Created by neil on 2018/3/13 0013.
  */
-
 @Qualifier
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
